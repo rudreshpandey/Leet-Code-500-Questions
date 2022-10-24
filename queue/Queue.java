d7c26fbf80f7da71de4700c88996bae414462d31
rudreshpandey <hvt_rudresh>
2022-10-24 15:13:32 +0530

    new code

--- queue/Queue.java
+++ queue/Queue.java
@@ -1,6 +1,21 @@
 package queue;
-
+import LikedList;
+import LikedList.SinglyLinkedList;
 public class Queue {
-   SinglyLinkedList list = new SinglyLinkedList<>();
+   public static void main(String[] args) {
+		
+	}
+   DoublyLinkedList<Integer> list = new DoublyLinkedList();
+   public int poll(){
+      list.removeFromFront();
+   }
+
+   public int peek(){
+      if(head==null) return -1;
+      else return head.data;
+   }
+    public add(int data){
+      
+    }
 
 }
